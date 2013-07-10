c3fce2a815ba9601c31766e55edf984cb4693158
Ben Arthur <arthurb@janelia.hhmi.org>
2013-07-10 09:26:52 -0400

    forgot a couple files

---
+++ av_update_video_params.m
@@ -0,0 +1,38 @@
+function data = av_update_video_params(vi)
+ss = getselectedsource(vi);
+a = get(ss);
+c = fieldnames(a);
+    
+data={}; j = 1;
+%ignore properties: parent, selected, tag, type, frameTimeout,
+for i = 1:length(c)
+    if strcmpi(c{i},'parent')|strcmpi(c{i},'selected')|strcmpi(c{i},'tag')|...
+       strcmpi(c{i},'type')|strcmpi(c{i},'NormalizedBytesPerPacket')|...
+       strcmpi(c{i},'FrameTimeout')
+        continue
+    end
+    data{j,1} = c{i}; data{j,2} = eval(['a.',c{i}]); 
+    
+    pinfo=propinfo(ss,c{i});
+    
+    switch pinfo.Constraint
+        case 'none'
+            data{j,3} = pinfo.ConstraintValue;
+        case 'bounded'
+            tmp =  pinfo.ConstraintValue;
+            data{j,3} = ['  [',num2str(tmp(1)),' ',num2str(tmp(2)),']'];
+        case 'enum'
+            str = '';
+            for i = 1:length(pinfo.ConstraintValue)
+                str = [str,', ',pinfo.ConstraintValue{i}];
+            end
+            str(1:2) = ' ';
+            data{j,3} = str;
+    end
+    
+    data{j,4}=pinfo.ReadOnly;
+    
+    j = j+1;
+end
+
+
---
+++ av_video_callback.m
@@ -0,0 +1,66 @@
+function av_video_callback(evt,src,vi,FPS,vifile,verbose,current)
+
+persistent time0
+
+tic;
+
+if verbose>0
+  disp('entering video_callback');
+end
+
+try
+  [data time metadata]=getdata(vi,FPS);
+catch
+  [data time metadata]=getdata(vi);
+end
+
+
+nframes=size(data,4);
+ts=nan(1,nframes);
+fn=nan(1,nframes);
+for i=1:nframes
+  %f=rgb2gray(data(:,:,:,i));
+
+  tmp=data(1,1:4,1,i);
+  second_count=uint16(bitand(tmp(1),254)/2);
+  cycle_count= uint16(bitand(tmp(1),1))*2^12 + ...
+               uint16(bitand(tmp(2),255))*2^4 + ...
+               uint16(bitand(tmp(3),128+64+32+16))/2^4;
+  cycle_offset=uint16(bitand(tmp(3),8+4+2+1)*2^8) + ...
+               uint16(bitand(tmp(4),255));
+  ts(i)=double(second_count) + double(cycle_count)/8000 + 125e-6*double(cycle_offset)/3072;
+
+  tmp=data(1,5:8,1,i);
+  fn(i)=uint32(tmp(1))*2^24 + uint32(tmp(2))*2^16 + uint32(tmp(3))*2^8 + uint32(tmp(4));
+end
+
+skip=round(diff(ts)*vifile.FrameRate);
+idx=find(skip>1);
+total=sum(skip(idx));
+if(~isempty(idx))
+  disp(['filling in ' num2str(total) ' skipped frames']);
+  data(1,1,1,end+total)=1;
+  for i=length(idx):-1:1
+    data(:,:,:, (idx(i)+1+skip(idx(i))) : (nframes+skip(idx(i))) ) = data(:,:,:, (idx(i)+1):nframes);
+    data(:,:,:, (idx(i)+1) : (idx(i)+skip(idx(i)))) = repmat(data(:,:,:,idx(i)),[1 1 1 skip(idx(i))]);
+    nframes=nframes+skip(idx(i));
+  end
+end
+writeVideo(vifile,data);
+
+if current
+  if(~isempty(time0))
+    assignin('base','FPSAchieved',round(length(time)/(time(end)-time0)));
+  end
+  time0=time(end);
+  assignin('base','FPSProcessed',round(FPS/toc));
+  assignin('base','FramesAvailable',get(vi,'FramesAvailable'));
+  assignin('base','FramesSkipped',total);
+end
+
+if verbose>0
+  disp(['exiting  video_callback:  ' ...
+        num2str(toc/(size(data,4)/FPS),3) 'x real time is ' ...
+        num2str(toc,3) 's to process ' ...
+        num2str(size(data,4)/FPS,3) 's of data']);
+end
