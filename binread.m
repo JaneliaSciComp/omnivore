1d8e4ccaa987fe7d0a4aa3208e8f4ee86007c93c
Ben Arthur <arthurb@hhmi.org>
2015-04-01 16:21:01 -0400

    added sample range to binread.m

--- binread.m
+++ binread.m
@@ -1,4 +1,15 @@
-function [y,fs,nbits]=binread(file)
+function [y,fs,nbits]=binread(file,varargin)
+% varargin can either be ommitted entirely,
+% or be a 2-vector of a sample range to return.
+% e.g. binread('foo.bin',[0 100]);
+
+if nargin>1
+    start=varargin{1}(1);
+    len=diff(varargin{1});
+else
+    start=0;
+    len=inf;
+end
 
 fid=fopen(file,'r');
 version=fread(fid,1,'double');
@@ -6,12 +17,14 @@ fs=fread(fid,1,'double');
 nchan=fread(fid,1,'double');
 switch version
     case 1
-        y=fread(fid,[nchan inf],'double');
+        fseek(fid,start,'cof');
+        y=fread(fid,[nchan len],'double');
         y=y';
         nbits=64;
         disp(['binread: version 1 (float64s), fs=' num2str(fs) ', nchan=' num2str(nchan) ', nsamples=' num2str(size(y,1))]);
     case 2
-        y=fread(fid,[nchan inf],'single');
+        fseek(fid,start,'cof');
+        y=fread(fid,[nchan len],'single');
         y=y';
         nbits=32;
         disp(['binread: version 2 (float32s), fs=' num2str(fs) ', nchan=' num2str(nchan) ', nsamples=' num2str(size(y,1))]);
@@ -19,7 +32,8 @@ switch version
         tmp=fread(fid,[2 nchan],'double');
         step=tmp(1,:);
         offset=tmp(2,:);
-        y=fread(fid,[nchan inf],'int16');
+        fseek(fid,start,'cof');
+        y=fread(fid,[nchan len],'int16');
         y=bsxfun(@times,y',step);
         y=bsxfun(@plus,y,offset);
         nbits=16;
